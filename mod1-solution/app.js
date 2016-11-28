3365ecdc549e8ef47bfa7d368f5820f0bcdc9110
Sergei Kovalenko <Sergei_Kovalenko@yahoo.com>
2016-11-27 23:27:12 -0500

    Solution to Module 1 by Sergei Kovalenko

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -13,19 +13,34 @@ function LunchCheckController ($scope) {
   $scope.lunchMenu = "";
   $scope.message = "";
 
-  $scope.getNumberOfItems = function () {
-    var i_numOfItems = splitMenuItems($scope.lunchMenu);
-    // console.log ("Lunch [" + $scope.lunchMenu + "] has " + i_numOfItems + " items.");
-    $scope.message = (i_numOfItems <= 3) ? "Enjoy!" : "Too much!";
-    return 0;
+  $scope.displayMessage = function () {
+    if ($scope.lunchMenu == "") {
+      $scope.message = "Please enter data first.";
+      return;
+    }
+    var i_numOfItems = getNumberOfItems($scope.lunchMenu);
+    //console.log ("Lunch [" + $scope.lunchMenu + "] has " + i_numOfItems + " items.");
+    if (i_numOfItems == 0) {
+      $scope.message = "Please enter data first.";
+    } else if (i_numOfItems < 4) {
+      $scope.message = "Enjoy!";
+    } else {
+      $scope.message = "Too much!";
+    }
+    return;
   };
 
 }
 
-  function splitMenuItems(s_menu){
+  function getNumberOfItems(s_menu){
     // empty items are not counted
     // e.g. "one, three,,,, two ,,   ,and four" counts as 4 items
-    return s_menu ? s_menu.replace(/\s|[,\s]+$/g, '').split( /[,\s]+|,+/ ).length : 0;
+    // console.log(s_menu.replace(/\s|[,\s]+$|^[,\s]+/g, '').length);
+
+    // remove all starting and training commas and all spaces
+    s_menu = s_menu.replace(/\s|[,\s]+$|^[,\s]+/g, '');
+
+    return s_menu.length > 0 ? s_menu.split( /[,\s]+|,+/ ).length : 0;
   }
 
 
